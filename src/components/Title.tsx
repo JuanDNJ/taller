eaddb207a2b944aec644be73c429023180c92a25
Juan Antonio <juandevnjv@gmail.com>
2026-03-28 21:37:53 +0100

    feat: agregar nuevo componente SVG Ok y refactorizar Title y TitlePage para incluir nuevas propiedades

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,17 +2,18 @@ import type { FC } from 'react'
 import { TAGS_SIZE, TITLE_SIZE } from '../config'
 
 type TitleProps = {
-  label: string,
-  tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
-  size?: 'xl' | 'lg' | 'md' | 'sm' | 'xs'
-  color?: string
+  label: string;
+  tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  size?: 'xl' | 'lg' | 'md' | 'sm' | 'xs';
+  color?: string;
+  className?: string;
 }
 
-const Title: FC<TitleProps> = ({ label, tag, size, color }) => {
+const Title: FC<TitleProps> = ({ label, tag, size, color, className  }) => {
   const Tag = tag || 'h1'
   return (
     <Tag
-      className={`${TAGS_SIZE[Tag]} ${color || 'text-title'} ${size && TITLE_SIZE[size]} font-bold`}
+      className={`${TAGS_SIZE[Tag]} ${color || 'text-title'} ${size && TITLE_SIZE[size]} font-bold ${className }`}
     >
       {label}
     </Tag>
